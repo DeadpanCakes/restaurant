67f5cfceaee1eacc9af771578a21bf1915e71627
Anthony Mendoza <anthony.mendoza310@gmail.com>
2020-03-16 16:26:00 -0700

    added elements via js and styled

---
+++ src/home.js
@@ -0,0 +1,21 @@
+const makeH1 = () => document.createElement("H1")
+const makeH3 = () => document.createElement("H3")
+const content = () => document.getElementById("content")
+
+const name = document.content.querySelector("H1")
+const displayName = name.textContent = "BEST FOOD PLACE";
+
+const copy = document.content.querySelector("H3")
+const displayCopy = copy.textContent = "--The best place to get the best food--"
+
+let x = 3
+
+function test (source) {
+    return `function makeDiv(){
+        document.createElement("DIV")
+    }`;
+}
+
+export { content }
+
+//export { makeH1, makeH3, x }
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("two more tests, please")
+const content = document.getElementById("content")
+const makeDiv = () => document.createElement("DIV")
+const makeH1 = () => document.createElement("H1")
+const makeH3 = () => document.createElement("H3")
+
+content.appendChild(makeH1())
+content.querySelector("h1").textContent = "RESTAURANT!"
+content.appendChild(makeH3())
+content.querySelector("h3").textContent = "It's very good! Yum!"
+//document.body.style.backgroundImage = "url('../img/homepage.png')"
+
+console.log(content)
